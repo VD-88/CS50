66d07182b0674c2b5ea3b20eca71beff0f0be098
Volodymyr Dukhanin <116935261+VD-88@users.noreply.github.com>
2023-05-30 20:24:50 +0300

    Add files via upload

---
+++ mario.c
@@ -0,0 +1,32 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int h;
+    do
+    {
+        h = get_int("height: ");
+    }
+    while (h < 1 || h > 8);
+
+
+    for (int i = 0; i < h; i++)
+    {
+        for (int k = 1; k < h - i; k++)
+        {
+            printf(" ");
+        }
+
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
